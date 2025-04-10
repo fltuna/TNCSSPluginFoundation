fec1e68d412dd3836df27722390a69c72c56df0e
faketuna <faketuna.st@gmail.com>
2025-04-10 20:06:38 +0900

    add #3: add CsTeamUtility.cs
    (cherry picked from commit 0c69601a6195fe83954a02d7e6716093391f5fac)

---
+++ TNCSSPluginFoundation/Utils/Entity/CsTeamUtility.cs
@@ -0,0 +1,54 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace TNCSSPluginFoundation.Utils.Entity;
+
+/// <summary>
+/// Utility class for manipulate CCSTeam entity
+/// </summary>
+public static class CsTeamUtility
+{
+    
+    /// <summary>
+    /// Set team name to specified string
+    /// </summary>
+    /// <param name="team">Team ID</param>
+    /// <param name="teamName">The name of team</param>
+    /// <returns>Returns true if successfully set. Otherwise false</returns>
+    public static bool SetTeamName(CsTeam team, string teamName)
+    {
+        var teamEntity = EntityUtil.GetTeam(team);
+        
+        if (teamEntity == null)
+            return false;
+        
+        teamEntity.Teamname = teamName;
+        Utilities.SetStateChanged(teamEntity, "CTeam", "m_szTeamname");
+        return true;
+    }
+
+
+    /// <summary>
+    /// Set team score to specified value
+    /// </summary>
+    /// <param name="team">Team ID</param>
+    /// <param name="score">The score of team</param>
+    /// <returns>Returns true if successfully set. Otherwise false</returns>
+    public static bool SetTeamScore(CsTeam team, int score)
+    {
+        var teamEntity = EntityUtil.GetTeam(team);
+        if (teamEntity == null)
+            return false;
+        
+        teamEntity.Score = score;
+        Utilities.SetStateChanged(teamEntity, "CTeam", "m_iScore");
+        return true;
+    }
+
+    
+    public static bool SetTeamLogo(CsTeam team, string logo)
+    {
+        // TODO(): Investigate logo behaviour and implement
+        return true;
+    }
+}
