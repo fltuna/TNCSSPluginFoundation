35f75477c19a1fa5421bfa72c9c5a73b6614ae8d
faketuna <faketuna.st@gmail.com>
2025-04-17 22:16:37 +0900

    modify: forget to increment version

--- TNCSSPluginFoundation.Example/TncssExamplePlugin.cs
+++ TNCSSPluginFoundation.Example/TncssExamplePlugin.cs
@@ -11,7 +11,7 @@ public sealed class TncssExamplePlugin: TncssPluginBase
 {
     // Same as CounterStrikeSharp plugin
     public override string ModuleName => "TNCSSExamplePlugin";
-    public override string ModuleVersion => "0.1.0";
+    public override string ModuleVersion => "0.1.2";
     
     // This is a base cfg directory path, but I'm not implemented anything yet.
     public override string BaseCfgDirectoryPath => ModuleDirectory;
