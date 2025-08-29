e4252831d320b0c4ff79e6cedc2dd2fe2270cc54
fltuna <47306029+fltuna@users.noreply.github.com>
2025-08-30 00:25:43 +0900

    add: implement ExtendedTargeting (#58)

---
+++ TNCSSPluginFoundation.Example/Modules/ExtendedTargetingTest/ExtenededTargetingModule.cs
@@ -0,0 +1,30 @@
+﻿using CounterStrikeSharp.API.Core;
+using TNCSSPluginFoundation.Extensions.Targeting;
+using TNCSSPluginFoundation.Models.Plugin;
+
+namespace TNCSSPluginFoundation.Example.Modules.ExtendedTargetingTest;
+
+public class ExtenededTargetingModule(IServiceProvider serviceProvider) : PluginModuleBase(serviceProvider)
+{
+    public override string PluginModuleName => "Extended Targeting";
+    public override string ModuleChatPrefix => "unused";
+    protected override bool UseTranslationKeyInModuleChatPrefix => false;
+
+    protected override void OnInitialize()
+    {
+        ExtendedTargeting.RegisterCustomParameterizedTarget("@hpge", IsPlayerGreaterThanHealth);
+    }
+
+    protected override void OnUnloadModule()
+    {
+        ExtendedTargeting.UnregisterCustomParameterizedTarget("@hpge");
+    }
+
+    private bool IsPlayerGreaterThanHealth(string param, CCSPlayerController player, CCSPlayerController? caller)
+    {
+        if (!int.TryParse(param, out int health))
+            return false;
+
+        return player.PlayerPawn.Value?.Health >= health;
+    }
+}
---
+++ TNCSSPluginFoundation/Models/Command/Validators/ExtendedTargetValidator.cs
@@ -0,0 +1,44 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+using TNCSSPluginFoundation.Extensions.Targeting;
+
+namespace TNCSSPluginFoundation.Models.Command.Validators;
+
+
+/// <summary>
+/// Extended targeting validator for TncssAbstractCommandBase <br/>
+/// Find players by using ExtendedTargeting. This validator fails when if no players found
+/// </summary>
+/// <param name="targetString">Target string</param>
+/// <param name="dontNotifyWhenFailed">When true, it will return TncssCommandValidationResult.FailedIgnoreDefault to avoid print default failure message</param>
+public class ExtendedTargetValidator(string targetString, bool dontNotifyWhenFailed = false): ICommandValidator
+{
+    /// <summary>
+    /// Name of this validator for identification purposes
+    /// </summary>
+    public string ValidatorName => "TncssBuiltinExtendedTargetValidator";
+
+    /// <summary>
+    /// Message of validation failure
+    /// </summary>
+    public string ValidationFailureMessage => "Common.Validation.Failure.ExtendedTarget";
+    
+    /// <summary>
+    /// Find players by using ExtendedTargeting. This validator fails when if no players found
+    /// </summary>
+    /// <param name="player">CCSPlayerController</param>
+    /// <param name="commandInfo">CommandInfo</param>
+    /// <returns>TncssCommandValidationResult</returns>
+    public TncssCommandValidationResult Validate(CCSPlayerController? player, CommandInfo commandInfo)
+    {
+        if (!ExtendedTargeting.ResolveExtendedTarget(targetString, player, out var foundTargets))
+        {
+            if (dontNotifyWhenFailed)
+                return TncssCommandValidationResult.FailedIgnoreDefault;
+            
+            return TncssCommandValidationResult.Failed;
+        }
+        
+        return TncssCommandValidationResult.Success;
+    }
+}
